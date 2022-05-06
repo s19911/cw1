4fee30c1f77a4ad65c4159fb54b92b83b008285e
CzestiOPpc <CzestiOPpc@DESKTOP-IMU12PD>
2022-05-06 08:06:56 +0200

    cw 1 dokonczone

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            var client = new HttpClient();
+            var result = await client.GetAsync(url);
+
+            if (result.IsSuccessStatusCode) 
+            {
+                var html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",
+                    RegexOptions.IgnoreCase);
+                var matches = regex.Matches(html);
+
+                foreach (var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+
+            }
         }
     }
 }
